ab54e4a767f42b908f244558a0d7bd33df1bc9dc
Егор Карташов <edkartashov@edu.hse.ru>
2021-06-14 18:47:15 +0500

    Commit latest migration

---
+++ ChatAppServer/Migrations/20210614125413_ChatroomNullableOwner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatAppServer.Migrations
+{
+    public partial class ChatroomNullableOwner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chatroom_User_OwnerId",
+                table: "Chatroom");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                table: "Chatroom",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chatroom_User_OwnerId",
+                table: "Chatroom",
+                column: "OwnerId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chatroom_User_OwnerId",
+                table: "Chatroom");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OwnerId",
+                table: "Chatroom",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chatroom_User_OwnerId",
+                table: "Chatroom",
+                column: "OwnerId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
